c2378288059da8ed888ef5e81408327fda1b1f00
G. A <100744523+bO-05@users.noreply.github.com>
2023-03-05 13:54:47 +0000

    update

--- src/routes/api/getRecommendation/+server.ts
+++ src/routes/api/getRecommendation/+server.ts
@@ -72,7 +72,7 @@ async function OpenAIStream(payload: OpenAIStreamPayload) {
 export async function POST({ request }: { request: any }) {
 	const { searched } = await request.json();
 	const payload = {
-		model: 'gpt-3.5-turbo',
+		model: 'text-davinci-003',
 		prompt: searched,
 		temperature: 0.7,
 		max_tokens: 2048,
